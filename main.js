429d9809db0392de9b50a517f90b50c4b864ddec
elliotfiske <elliotfiske@gmail.com>
2016-12-10 18:16:35 -0800

    access control

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ app.use(function(req, res, next) {
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
