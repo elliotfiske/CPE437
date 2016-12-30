d018ce4968e6e7cc71917d7f0e7778dfba106dfd
elliotfiske <elliotfiske@gmail.com>
2016-12-30 00:09:16 -0800

    Enrollments pulled to separate file. All is right with the world (golden)

---
+++ Routes/Course/Enrollment/enrollments.js
@@ -0,0 +1,75 @@
+var Express = require('express');
+var Promise = require('bluebird');
+var connections = require('../../Connections.js');
+var sequelize = require('../../sequelize.js');
+var Tags = require('../../Validator.js').Tags;
+var doErrorResponse = require('../../Validator.js').doErrorResponse;
+var router = Express.Router({caseSensitive: false});
+var updateStreak = require('../../middleware.js').updateStreak;
+router.baseURL = '/enrs';
+
+router.post('/', function(req, res) {
+   var vld = req.validator;
+   var prs = req.session;
+
+   return vld.hasFields(req.body, ['prsId'])
+   .then(function() {
+      return sequelize.Person.findById(req.body.prsId);
+   })
+   .then(function(person) {
+      return vld.check(person, Tags.notFound, null, person, "Couldn't find a user with ID " + req.body.prsId);
+   })
+   .then(function(person) {
+
+      return vld.check(prs.isAdmin() || prs.id === req.body.prsId || // Are you Admin, enrolling yourself,
+      req.course.ownerId === prs.id, Tags.noPermission) // or the teacher of this course?
+      .then(function() {
+         return person.hasClass(req.course)
+      })
+      .then(function(alreadyAdded) {
+         return vld.check(!alreadyAdded, Tags.dupName, null, null, "Student is already enrolled for that class.");
+      })
+      .then(function() {
+         return person.addClass(req.course);
+      })
+      .then(function() {
+         return person.getClasses();
+      })
+      .then(function(classes) {
+         res.json(classes).end();
+      });
+   })
+   .catch(doErrorResponse(res));
+});
+
+router.get('/', function(req, res) {
+   var vld = req.validator;
+   var prs = req.session;
+
+   return vld.checkPrsOK(req.course.ownerId)
+   .then(function() {
+      return req.course.getEnrolledDudes();
+   })
+   .then(function(dudes) {
+      res.json(dudes);
+   })
+   .catch(doErrorResponse(res));
+});
+
+router.delete('/:name/enrs/:enrId', function(req, res) {
+   var vld = req.validator;
+   var prs = req.session;
+
+   return vld.checkAdmin()
+   .then(function() {
+      return sequelize.Enrollment.findOne({
+         where: {personId: req.session.id, courseName: req.course.sanitizedName}
+      });
+   })
+   .then(function(enr) {
+      return enr.destroy();
+   })
+   .catch(doErrorResponse(res));
+});
+
+module.exports = router;
--- Routes/Course/courses.js
+++ Routes/Course/courses.js
@@ -25,35 +25,11 @@ function getCourseModel(req, res, next) {
   .catch(doErrorResponse(res));
 }
 
-// This is called any time when we need to display the streak. This will happen
-//  when we look at a course page, or when we complete a challenge. Make sure
-//  you call getCourseModel somewhere before
-// var updateStreak = function(req, res, next) {
-//    if (!req.course) {
-//       console.error("You didn't put the getCourseModel middleware before this middleware!");
-//       res.sendStatus(500);
-//    }
-//
-//    return sequelize.Enrollment.findOne({
-//       where: {personId: req.session.id, courseName: req.course.sanitizedName}
-//    })
-//    .then(function(enr) {
-//       // When was the last time the user made an attempt?
-//       var now  = Date.now();
-//       var then = enr.lastStreakTime.getTime();
-//       var DAY_MS = 86400000;
-//
-//       if (now - then > DAY_MS) { // 2 far in the past 4 u
-//          return enr.updateAttributes({
-//             streak: 0
-//          });
-//       }
-//    })
-//    .catch(doErrorResponse(res));
-// }
-
 var challengeRouter = require('./Challenge/challenges.js');
 router.use('/:courseName/challenge', getCourseModel, challengeRouter);
+
+var enrollmentsRouter = require('./Enrollment/enrollments.js');
+router.use('/:courseName/enrs', getCourseModel, enrollmentsRouter);
 
 router.get('/', function(req, res) {
   var vld = req.validator;
@@ -76,8 +52,7 @@ router.post('/', function(req, res) {
    .then(function() {
       return sequelize.Person.findOne({where: {email: req.body.owner}})
       .then(function(teacher) {
-         return vld.check(teacher && teacher.role >= 1, Tags.notFound, null, teacher,
-            "No teacher found for email " + req.body.owner);
+         return vld.check(teacher && teacher.role >= 1, Tags.notFound, null, teacher, "No teacher found for email " + req.body.owner);
       })
       .then(function(teacher) {
          return sequelize.Course.create({
@@ -146,128 +121,13 @@ router.get('/:courseName', getCourseModel, updateStreak, function(req, res) {
       raw: true
    })
    .then(function(enr) {
-      req.course.Enrollments = [enr];
-      res.json(req.course);
+      var courseResult = req.course.get({plain: true});
+      courseResult.Enrollments = [enr];
+      res.json(courseResult);
    })
    .catch(doErrorResponse(res));
 });
 
-router.post('/:name/enrs', function(req, res) {
-  var vld = req.validator;
-  var prs = req.session;
-
-  var getPerson = sequelize.Person.findById(req.body.prsId);
-  var getCourse = sequelize.Course.findOne({ where: {sanitizedName: req.params.name} });
-
-  vld.hasFields(req.body, ['prsId'])
-  .then(function() {
-    return Promise.all([getPerson, getCourse]);
-  })
-  .spread(function(person, course) {
-    if (!person || !course) {
-      return Promise.reject({tag: Tags.notFound});
-    }
-
-    return vld.check(prs.isAdmin() || prs.id === req.body.prsId || // Are you Admin, enrolling yourself,
-    course.ownerId === prs.id, Tags.noPermission) // or the teacher of this course?
-    .then(function() {
-      return person.hasClass(course)
-    })
-    .then(function(alreadyAdded) {
-      return vld.check(!alreadyAdded, Tags.dupName);
-    })
-    .then(function() {
-      return person.addClasses([course]);
-    })
-    .then(function() {
-      return person.getClasses();
-    })
-    .then(function(classes) {
-      res.json(classes).end();
-    });
-  })
-  .catch(doErrorResponse(res));
-});
-
-router.get('/:courseName/enrs', getCourseModel, function(req, res) {
-   var vld = req.validator;
-   var prs = req.session;
-
-   return vld.checkPrsOK(req.course.ownerId)
-   .then(function() {
-      return req.course.getEnrolledDudes();
-   })
-   .then(function(dudes) {
-      res.json(dudes);
-   })
-   .catch(doErrorResponse(res));
-});
-
-router.get('/:name/enrs/:enrId', function(req, res) {
-  var vld = req._validator;
-  var prs = req.session;
-
-  connections.getConnection(res, function(cnn) {
-    function getResult(needsIdCheck) {
-      var queryArr = [
-        'SELECT enrId, whenEnrolled, prsId, lastName, firstName',
-        'FROM Enrollment enr INNER JOIN Person p ON p.id = prsId',
-        'WHERE enr.enrId = ? AND enr.courseName = ?'
-      ];
-      cnn.query(queryArr.join(' '), [req.params.enrId, req.params.name], function(err, result) {
-        if (vld.check(result && result.length, Tags.notFound) &&
-        (!needsIdCheck || vld.check(result[0].prsId === prs.id, Tags.noPermission)))
-        res.json(result[0]);
-
-        cnn.release();
-      });
-    }
-
-    if (prs.isAdmin()) {
-      getResult(false);
-    }
-    else if (prs.isTeacher()) {
-      cnn.query('SELECT ownerId FROM Course WHERE name = ?', [req.params.name], function(err, result) {
-        if (result && result[0].ownerId === prs.id) {
-          getResult(false);
-        }
-        else
-        getResult(true);
-      });
-    }
-    else
-    getResult(true);
-  });
-});
-
-router.delete('/:name/enrs/:enrId', function(req, res) {
-  var vld = req._validator;
-  var prs = req.session;
-
-  if (vld.checkAdmin()) {
-    connections.getConnection(res, function(cnn) {
-      function doDelete() {
-        cnn.query('DELETE FROM Enrollment WHERE id = ?', [req.params.enrId], function(err, result) {
-          res.end();
-          cnn.release();
-        });
-      }
-
-      if (prs.isAdmin()) {
-        doDelete();
-      }
-      else {
-        cnn.query('SELECT ownerId FROM Course WHERE name = ?', [req.params.name], function(err, result) {
-          if (vld.check(result && result[0].ownerId === prs.id, Tags.noPermission)) {
-            doDelete();
-          }
-          else
-          cnn.release();
-        });
-      }
-    });
-  }
-});
 
 router.get('/:name/chls', function(req, res) {
   var vld = req.validator;
