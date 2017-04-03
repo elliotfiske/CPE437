33797f02e0337accf7a353792acd9c90c0ccfaf0
elliotfiske <elliotfiske@gmail.com>
2017-04-03 12:56:08 -0700

    deploy ahoy

--- public/Home/homeController.js
+++ public/Home/homeController.js
@@ -97,7 +97,7 @@ app.controller('homeController', ['$scope', '$state', 'login', '$rootScope', 'ap
          var user = reponse.data[0];
          localStorage.user = JSON.stringify(user);
          $rootScope.loggedUser = user;
-         return user;
+         $state.go('home');
       })
       .catch(toastr.doErrorMessage(function(err) {}));
    }
